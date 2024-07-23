3930e2d8e1ff3253211b58ee3bb7786accd02e0a
Joao Moraes <joaomcarril@gmail.com>
2024-07-22 22:41:32 -0300

    Revertir "Corrección de líneas con conflicto y eliminación de archivos innecesarios."
    Esto revierte la confirmación 3c5a25622e46fc0dc808e5878e20a1a884d71d2c.

--- src/main/java/persistencia/Conexion.java
+++ src/main/java/persistencia/Conexion.java
@@ -19,10 +19,17 @@ public class Conexion {
     public Ciudad obtenerCiudad(int idCiudad) {
         Ciudad c = new Ciudad();
         c.setIdCiudad(idCiudad);
+<<<<<<< HEAD
+        c.setImagen("Cv_Montevideo.jpg");
+        c.setDescripcion("Montevideo, descripción de la ciudad...");
+        c.setNombre(" Montevideo");
+
+=======
         c.setImagen("CV_Montevideo.jpg");
         c.setDescripcion("El barrio histórico de Montevideo, con calles adoquinadas y arquitectura colonial...");
         c.setNombre(" Montevideo...");
         
+>>>>>>> d8d8646f557bea175a8248e0d82492552da37a3e
         return c;
     }
 
